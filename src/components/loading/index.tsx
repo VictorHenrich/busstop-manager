e8f5b17d4e9debd12d7b6b4530fc5e3af520067e
VictorHenrich <victorhenrich993@gmail.com>
2024-07-05 20:52:13 -0300

    Aplicando melhorias

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -16,6 +16,7 @@ export default function AppLoading({
                 background="rgba(0, 0, 0, 0.8)"
                 position="fixed"
                 top={0}
+                left={0}
             >
                 <Spinner
                     size="xl"
